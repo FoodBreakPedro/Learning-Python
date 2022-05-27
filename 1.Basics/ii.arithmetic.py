8bd781dff935948461eac2a8b0d429660d6de8d7
Pedro R Zabala <pedro.r.zabala@gmail.com>
2022-05-26 20:31:17 -0400

    test 1

---
+++ 1.Basics/ii.arithmetic.py
@@ -0,0 +1,20 @@
+a = 5
+b = 2
+
+# add 5 + 2  prints 7
+print(a+b)
+
+# substraction, 5 - 2 prints 3
+print(a-b)
+
+#integer division, 5//2 prints 2
+print(a//b)
+
+#float division, 5/2 prints 2.5
+print(a/b)
+
+#remainder, 5 % 2, prints 1
+print(a % b) 
+
+#multiplication 5 * 2 prints 10
+print(a*b)
