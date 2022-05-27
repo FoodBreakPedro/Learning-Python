3fec0d26a435e856a5895f0600c974800bc64ece
Pedro R Zabala <pedro.r.zabala@gmail.com>
2022-05-26 20:46:36 -0400

    added multi line and variable prints to hello, world

--- 1.Basics/i.hello_world.py
+++ 1.Basics/i.hello_world.py
@@ -1,1 +1,18 @@
+#prints a single line
 print("Hello, World!")
+
+#prints multiple lines
+print("""
+This 
+is
+Exciting!
+""")
+
+#prints line with variables
+name = "Pedro"
+print ("Hello, my name is %s " % name)
+
+#print value from user
+standard_input = 'Ava' #for testing this needs to go before the input
+value = input("What word would you like to print out?:")
+print("The word you selected was %s. Thank you for your input" % value)
